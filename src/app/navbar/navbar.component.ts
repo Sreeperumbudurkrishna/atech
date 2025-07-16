1ab3943ee98ce9550a4fa29c922272f744b3f36c
Krishna Vamshi <Sreeperumbudurkrishna@gmail.com>
2025-07-16 10:48:14 -0500

    Modified Navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+navbar={
+  item1:"Home",
+  item2:"Restaurant",
+  item3:"About Us",
+  item4:"Contact Us",
+  item5:"Sign In",
+  src:"https://png.pngtree.com/png-clipart/20220823/original/pngtree-food-logo-png-image_8466881.png",
+  alt:"Networkdown"
+}
 
 }
