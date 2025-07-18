2a51d0d253fc8f64f9e1e904bb305eee8d3fa20c
Krishna Vamshi <Sreeperumbudurkrishna@gmail.com>
2025-07-18 09:17:05 -0500

    updated

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./banner.component.css']
 })
 export class BannerComponent {
-
+banner={
+  img1:"https://images.pexels.com/photos/9627765/pexels-photo-9627765.jpeg",
+  img2:"https://images.pexels.com/photos/7220992/pexels-photo-7220992.jpeg" ,
+  img3:"https://images.pexels.com/photos/7219243/pexels-photo-7219243.jpeg",
+  alt:"check your network connection"
 }
+}
+
+
--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./cards.component.css']
 })
 export class CardsComponent {
-
+  cards=[
+    {
+      src:"https://images.pexels.com/photos/699953/pexels-photo-699953.jpeg",
+      alt:"Italian",
+      title:"Italian",
+      price:"200",
+      button:""
+    },
+    {
+      src:"https://images.pexels.com/photos/842571/pexels-photo-842571.jpeg",
+      alt:"Italian",
+      title:"Italian",
+      price:"200",
+      button:""
+    },
+    
+  ]
 }
